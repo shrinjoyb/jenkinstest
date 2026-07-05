a3fae4e7c3d496d5e2f2b6af902bb9c172c89c6e
Shrinjoy <Shrinjoy@gmail.com>
2026-07-05 13:32:26 +0530

    This is my 2nd commit.....

--- StudentGradeCalculator.java
+++ StudentGradeCalculator.java
@@ -1,48 +1,34 @@
-import java.util.Scanner;
-
 public class StudentGradeCalculator {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        String name = "Shrinjoy";
 
-        System.out.print("Enter Student Name: ");
-        String name = sc.nextLine();
-
-        System.out.print("Enter Marks in Java: ");
-        int java = sc.nextInt();
-
-        System.out.print("Enter Marks in SQL: ");
-        int sql = sc.nextInt();
-
-        System.out.print("Enter Marks in Testing: ");
-        int testing = sc.nextInt();
+        int java = 90;
+        int sql = 85;
+        int testing = 95;
 
         int total = java + sql + testing;
         double average = total / 3.0;
 
         String grade;
 
-        if (average >= 90) {
+        if (average >= 90)
             grade = "A+";
-        } else if (average >= 80) {
+        else if (average >= 80)
             grade = "A";
-        } else if (average >= 70) {
+        else if (average >= 70)
             grade = "B";
-        } else if (average >= 60) {
+        else if (average >= 60)
             grade = "C";
-        } else if (average >= 40) {
+        else if (average >= 40)
             grade = "D";
-        } else {
+        else
             grade = "Fail";
-        }
 
-        System.out.println("\n===== Student Report =====");
-        System.out.println("Name    : " + name);
-        System.out.println("Total   : " + total);
-        System.out.println("Average : " + average);
-        System.out.println("Grade   : " + grade);
-
-        sc.close();
+        System.out.println("Student Name : " + name);
+        System.out.println("Total Marks  : " + total);
+        System.out.println("Average      : " + average);
+        System.out.println("Grade        : " + grade);
     }
 }
