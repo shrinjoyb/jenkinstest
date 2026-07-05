d908c7e4845acd3d048966e1518ee7eb84905135
Shrinjoy <Shrinjoy@gmail.com>
2026-07-05 11:41:08 +0530

    this is a dummy project for jenkins

---
+++ Calculator.java
@@ -0,0 +1,16 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 10;
+
+        System.out.println("First Number  : " + a);
+        System.out.println("Second Number : " + b);
+
+        System.out.println("Addition       : " + (a + b));
+        System.out.println("Subtraction    : " + (a - b));
+        System.out.println("Multiplication : " + (a * b));
+        System.out.println("Division       : " + (a / b));
+    }
+}
